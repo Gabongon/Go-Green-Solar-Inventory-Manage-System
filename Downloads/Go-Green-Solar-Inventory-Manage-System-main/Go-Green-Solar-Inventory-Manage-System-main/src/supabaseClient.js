2f4c13ef54097ed53a1f98b49b2593432f9999b5
OwOner <82586349+OwOner@users.noreply.github.com>
2026-03-06 15:41:59 +0800

    Delete Downloads/Go-Green-Solar-Inventory-Manage-System-main/Go-Green-Solar-Inventory-Manage-System-main directory

--- Downloads/Go-Green-Solar-Inventory-Manage-System-main/Go-Green-Solar-Inventory-Manage-System-main/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Accessing the environment variables from your .env file
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Initializing the Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
